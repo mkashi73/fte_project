f387d7a6ae3ce527b066e6187f687c70d6f66dcc
mkashi73 <53524191+mkashi73@users.noreply.github.com>
2025-01-20 12:49:04 +0500

    aa

--- assets/template/pages/products/products.js
+++ assets/template/pages/products/products.js
@@ -1,323 +1,4 @@
 
-
-$(document).on('click', '.cnClipReport', (e) => {
-  // e.preventDefault()
-
-  // var productId = e.target.dataset.pid
-
-  // console.log( e.target.dataset.pid )
-
-  // let page = $('.active').children().html() 
-  
-  // if ( typeof page === "undefined" ) 
-  // {
-  //   page = 1
-  // }
-
-  // $.ajax({
-  //   'url' : base_url + 'get/product/page/' + page,
-  //   'data' : $(this).serializeArray(),
-  //   'method' : 'POST',
-  //   success : function ( res ) {
-  //     let data = JSON.parse( res )
-
-  //     $(".productList").html(data.ProductList)
-  //     $(".productPaginationLink").html(data.ProductPaginationLink)
-  //   }
-  // })
-  
-  
-})
-
-
-
-// OPTION SETTING 
-$(function () {
-  $('.countrySearch').select2({
-    width: '100%',
-    placeholder: 'Select Country',
-  })
-
-  $('.stateSearch').select2({
-    placeholder: 'Select State',
-    width: '100%',
-  })
-
-  $('.citySearch').select2({
-    placeholder: 'Select City',
-    width: '100%',
-  })  
-
-// for clients select2
-  $('.clientSearch').select2({
-    placeholder: 'Select Client',
-    width: '100%',
-  })  
-
-
-  $('.clientSearch').on('change', function(e) {
-    var id = $(this).val();
-    // alert(id);
-     window.productID = id
-
-    let data = {
-      'c_id' : id
-    }
-
-      $.ajax({
-        url : base_url + "auto/get/client/" + id,
-        method : "GET",
-        dataType : "JSON",
-        beforeSend : function() {
-          
-        },
-        success : function (data) 
-        {
-          
-          // CONSIGNEE PHONE NUMBER
-          if ( data.code == 200 ) {
-
-              $('#C_NAME').val(data.record.CLIENT_NAME)
-              $('#C_EMAIL').val(data.record.CLIENT_EMAIL)
-              $('#C_MOBILE').val(data.record.CLIENT_MOBILE)
-              $('#C_ADDRESS').val(data.record.ADDRESS)
-              $('#C_CNIC').val(data.record.CNIC)
-              $('#C_CODE').val(data.record.ZIP_CODE)
-              // $('#C_COUNTRY').val(data.record.COUNTRY_ID)
-              // $('#C_CITY').val(data.record.CITY_ID)
-              // $('#C_STATE').val(data.record.STATE_ID)
-          if ( data.record.COUNTRY_ID)
-          {
-            $('#GetCountry').val(data.record.COUNTRY_ID)
-
-            $('#GetCountry').trigger('change')
-          }
-          else
-          {
-            $('#GetState').empty()
-            $('#GetCity').empty()
-          }
-
-          }else{
-            data.message
-          }
-        
-        }
-      })
-  })
-
-// $(function(){
-    
-
-    $('#GetCountry').on('change', function( e ){
-
-      var id = $(this).val();
-
-      // alert(id);
-
-      // to check client id is empty or not
-          let client = $('#select-client').val();
-          if (client !='') {
-
-
-    var record = {
-        c_id : window.productID
-      }
-      
-      $.ajax({
-        url : base_url + "client/state/get/data/" + id + '/' + window.productID,
-        method : "GET",
-        dataType : "JSON",
-        success : function (data) 
-        {
-          // console.log(data)
-          $('#GetState').empty();
-          
-          $('#GetState').append('<option>Select State</option>')
-
-          if ( data.record )
-          {
-            $.each(data.record, function ( index , value ){
-              if ( value.STATE_ID == data.productRecord[0].STATE_ID )
-              {
-                $('#GetState').append('<option value="' + value.STATE_ID + '" selected="selected" >' + value.STATE + '</option>')
-              }
-              else{
-                $('#GetState').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
-              }
-            })
-
-            $('#GetState').trigger('change')
-
-          }
-        }
-
-      })
-    }
-      }) // END OF ON CHANGE COUNTRY EVENT
-
-
-      $('#GetState').on('change', function( e ){
-
-        var id = $(this).val();
-
-          // alert(id);
-
-        var record = {
-            c_id : window.productID
-          }
-          // to check if client id is empty or not
-    let client = $('#select-client').val();
-    if (client !='') {
-        
-        $.ajax({
-          url : base_url + "client/city/get/data/" + id + '/' + window.productID,
-          method : "GET",
-          dataType : "JSON",
-          success : function (data) 
-          {
-
-            $('#GetCity').empty();
-
-            $('#GetCity').append('<option>Select City</option>')
-
-            $.each(data.record, function ( index , value ){
-              // console.log( value.CITY_ID )
-              if ( value.CITY_ID == data.productRecord[0].CITY_ID )
-              {
-                $('#GetCity').append('<option value="' + value.CITY_ID + '" selected="selected" >' + value.CITY + '</option>')
-              }
-              else{
-                $('#GetCity').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
-              }
-              // $('#updateShipperCity').append('<option value="' + value.CITY_ID + '" >' + value.CITY + '</option>')
-            })
-          }
-        })
-      
-
-      }
-          // end of checking
-
-      }) // END OF ON CHANGE COUNTRY EVENT
-
-// })
-
-
-
-
-  // GET STATE VIA COUNTRY CHANGE EVENT
-  $('.recieverCountrySearch').on('change', function ( e ) {
-
-    var id = $(this).val();
-    let client = $('#select-client').val();
-    if (client =='') {
-
-    $.ajax({
-      url : base_url + "state/get/data/" + id,
-      method : "GET",
-      dataType : "JSON",
-      beforeSend : function() {
-        
-      },
-      success : function (data) 
-      {
-        $('.recieverStateSearch').empty();
-
-        $('.recieverStateSearch').append('<option><-- Select State --></option>')
-
-        $.each(data.record, function ( index , value ){
-          $('.recieverStateSearch').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
-        })
-      }
-    })
-  }
-  }) // END OF ON CHANGE COUNTRY EVENT
-
-  // GET CITY VIA STATE CHANGE EVENT
-  $('.recieverStateSearch').on('change', function ( e ) {
-
-    var id = $(this).val();
-    let client = $('#select-client').val();
-    if (client =='') {
-    $.ajax({
-      url : base_url + "city/get/data/" + id,
-      method : "GET",
-      dataType : "JSON",
-      beforeSend : function() {
-        
-      },
-      success : function (data) 
-      {
-        $('.receiverCitySearch').empty();
-
-        $('.receiverCitySearch').append('<option><-- Select City --></option>')
-
-        $.each(data.record, function ( index , value ){
-          $('.receiverCitySearch').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
-        })
-      }
-    })
-     }
-  }) // END OF ON CHANGE STATE EVENT
-
-
-  // GET STATE VIA COUNTRY CHANGE EVENT
-  $('.senderCountrySearch').on('change', function ( e ) {
-
-    var id = $(this).val();
-    let client = $('#select-client').val();
-    
-    $.ajax({
-      url : base_url + "state/get/data/" + id,
-      method : "GET",
-      dataType : "JSON",
-      beforeSend : function() {
-        
-      },
-      success : function (data) 
-      {
-        $('.senderStateSearch').empty();
-
-        $('.senderStateSearch').append('<option><-- Select State --></option>')
-
-        $.each(data.record, function ( index , value ){
-          $('.senderStateSearch').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
-        })
-      }
-    })
-  
-  }) // END OF ON CHANGE COUNTRY EVENT
-
-  // GET STATE VIA COUNTRY CHANGE EVENT
-  $('.senderStateSearch').on('change', function ( e ) {
-
-    var id = $(this).val();
-
-    $.ajax({
-      url : base_url + "city/get/data/" + id,
-      method : "GET",
-      dataType : "JSON",
-      beforeSend : function() {
-        
-      },
-      success : function (data) 
-      {
-        $('.senderCitySearch').empty();
-
-        $('.senderCitySearch').append('<option><-- Select City --></option>')
-
-        $.each(data.record, function ( index , value ){
-          $('.senderCitySearch').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
-        })
-      }
-    })
-  }) // END OF ON CHANGE COUNTRY EVENT
-
-  
-
-
-}) // END OF MAIN DOCUMENT FUNCTION
 /*****************************
 AUTHOR : SALMAN BUKHARI
 DATE   : 04/01/2019
@@ -1254,3 +935,325 @@ $(document).on('submit', '#search-product-cn-number', function (e) {
   
   
   
+
+
+  
+
+$(document).on('click', '.cnClipReport', (e) => {
+  // e.preventDefault()
+
+  // var productId = e.target.dataset.pid
+
+  // console.log( e.target.dataset.pid )
+
+  // let page = $('.active').children().html() 
+  
+  // if ( typeof page === "undefined" ) 
+  // {
+  //   page = 1
+  // }
+
+  // $.ajax({
+  //   'url' : base_url + 'get/product/page/' + page,
+  //   'data' : $(this).serializeArray(),
+  //   'method' : 'POST',
+  //   success : function ( res ) {
+  //     let data = JSON.parse( res )
+
+  //     $(".productList").html(data.ProductList)
+  //     $(".productPaginationLink").html(data.ProductPaginationLink)
+  //   }
+  // })
+  
+  
+})
+
+
+
+// OPTION SETTING 
+$(function () {
+  $('.countrySearch').select2({
+    width: '100%',
+    placeholder: 'Select Country',
+  })
+
+  $('.stateSearch').select2({
+    placeholder: 'Select State',
+    width: '100%',
+  })
+
+  $('.citySearch').select2({
+    placeholder: 'Select City',
+    width: '100%',
+  })  
+
+// for clients select2
+  $('.clientSearch').select2({
+    placeholder: 'Select Client',
+    width: '100%',
+  })  
+
+
+  $('.clientSearch').on('change', function(e) {
+    var id = $(this).val();
+    // alert(id);
+     window.productID = id
+
+    let data = {
+      'c_id' : id
+    }
+
+      $.ajax({
+        url : base_url + "auto/get/client/" + id,
+        method : "GET",
+        dataType : "JSON",
+        beforeSend : function() {
+          
+        },
+        success : function (data) 
+        {
+          
+          // CONSIGNEE PHONE NUMBER
+          if ( data.code == 200 ) {
+
+              $('#C_NAME').val(data.record.CLIENT_NAME)
+              $('#C_EMAIL').val(data.record.CLIENT_EMAIL)
+              $('#C_MOBILE').val(data.record.CLIENT_MOBILE)
+              $('#C_ADDRESS').val(data.record.ADDRESS)
+              $('#C_CNIC').val(data.record.CNIC)
+              $('#C_CODE').val(data.record.ZIP_CODE)
+              // $('#C_COUNTRY').val(data.record.COUNTRY_ID)
+              // $('#C_CITY').val(data.record.CITY_ID)
+              // $('#C_STATE').val(data.record.STATE_ID)
+          if ( data.record.COUNTRY_ID)
+          {
+            $('#GetCountry').val(data.record.COUNTRY_ID)
+
+            $('#GetCountry').trigger('change')
+          }
+          else
+          {
+            $('#GetState').empty()
+            $('#GetCity').empty()
+          }
+
+          }else{
+            data.message
+          }
+        
+        }
+      })
+  })
+
+// $(function(){
+    
+
+    $('#GetCountry').on('change', function( e ){
+
+      var id = $(this).val();
+
+      // alert(id);
+
+      // to check client id is empty or not
+          let client = $('#select-client').val();
+          if (client !='') {
+
+
+    var record = {
+        c_id : window.productID
+      }
+      
+      $.ajax({
+        url : base_url + "client/state/get/data/" + id + '/' + window.productID,
+        method : "GET",
+        dataType : "JSON",
+        success : function (data) 
+        {
+          // console.log(data)
+          $('#GetState').empty();
+          
+          $('#GetState').append('<option>Select State</option>')
+
+          if ( data.record )
+          {
+            $.each(data.record, function ( index , value ){
+              if ( value.STATE_ID == data.productRecord[0].STATE_ID )
+              {
+                $('#GetState').append('<option value="' + value.STATE_ID + '" selected="selected" >' + value.STATE + '</option>')
+              }
+              else{
+                $('#GetState').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
+              }
+            })
+
+            $('#GetState').trigger('change')
+
+          }
+        }
+
+      })
+    }
+      }) // END OF ON CHANGE COUNTRY EVENT
+
+
+      $('#GetState').on('change', function( e ){
+
+        var id = $(this).val();
+
+          // alert(id);
+
+        var record = {
+            c_id : window.productID
+          }
+          // to check if client id is empty or not
+    let client = $('#select-client').val();
+    if (client !='') {
+        
+        $.ajax({
+          url : base_url + "client/city/get/data/" + id + '/' + window.productID,
+          method : "GET",
+          dataType : "JSON",
+          success : function (data) 
+          {
+
+            $('#GetCity').empty();
+
+            $('#GetCity').append('<option>Select City</option>')
+
+            $.each(data.record, function ( index , value ){
+              // console.log( value.CITY_ID )
+              if ( value.CITY_ID == data.productRecord[0].CITY_ID )
+              {
+                $('#GetCity').append('<option value="' + value.CITY_ID + '" selected="selected" >' + value.CITY + '</option>')
+              }
+              else{
+                $('#GetCity').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
+              }
+              // $('#updateShipperCity').append('<option value="' + value.CITY_ID + '" >' + value.CITY + '</option>')
+            })
+          }
+        })
+      
+
+      }
+          // end of checking
+
+      }) // END OF ON CHANGE COUNTRY EVENT
+
+// })
+
+
+
+
+  // GET STATE VIA COUNTRY CHANGE EVENT
+  $('.recieverCountrySearch').on('change', function ( e ) {
+
+    var id = $(this).val();
+    let client = $('#select-client').val();
+    if (client =='') {
+
+    $.ajax({
+      url : base_url + "state/get/data/" + id,
+      method : "GET",
+      dataType : "JSON",
+      beforeSend : function() {
+        
+      },
+      success : function (data) 
+      {
+        $('.recieverStateSearch').empty();
+
+        $('.recieverStateSearch').append('<option><-- Select State --></option>')
+
+        $.each(data.record, function ( index , value ){
+          $('.recieverStateSearch').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
+        })
+      }
+    })
+  }
+  }) // END OF ON CHANGE COUNTRY EVENT
+
+  // GET CITY VIA STATE CHANGE EVENT
+  $('.recieverStateSearch').on('change', function ( e ) {
+
+    var id = $(this).val();
+    let client = $('#select-client').val();
+    if (client =='') {
+    $.ajax({
+      url : base_url + "city/get/data/" + id,
+      method : "GET",
+      dataType : "JSON",
+      beforeSend : function() {
+        
+      },
+      success : function (data) 
+      {
+        $('.receiverCitySearch').empty();
+
+        $('.receiverCitySearch').append('<option><-- Select City --></option>')
+
+        $.each(data.record, function ( index , value ){
+          $('.receiverCitySearch').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
+        })
+      }
+    })
+     }
+  }) // END OF ON CHANGE STATE EVENT
+
+
+  // GET STATE VIA COUNTRY CHANGE EVENT
+  $('.senderCountrySearch').on('change', function ( e ) {
+
+    var id = $(this).val();
+    let client = $('#select-client').val();
+    
+    $.ajax({
+      url : base_url + "state/get/data/" + id,
+      method : "GET",
+      dataType : "JSON",
+      beforeSend : function() {
+        
+      },
+      success : function (data) 
+      {
+        $('.senderStateSearch').empty();
+
+        $('.senderStateSearch').append('<option><-- Select State --></option>')
+
+        $.each(data.record, function ( index , value ){
+          $('.senderStateSearch').append('<option value="' + value.STATE_ID + '">' + value.STATE + '</option>')
+        })
+      }
+    })
+  
+  }) // END OF ON CHANGE COUNTRY EVENT
+
+  // GET STATE VIA COUNTRY CHANGE EVENT
+  $('.senderStateSearch').on('change', function ( e ) {
+
+    var id = $(this).val();
+
+    $.ajax({
+      url : base_url + "city/get/data/" + id,
+      method : "GET",
+      dataType : "JSON",
+      beforeSend : function() {
+        
+      },
+      success : function (data) 
+      {
+        $('.senderCitySearch').empty();
+
+        $('.senderCitySearch').append('<option><-- Select City --></option>')
+
+        $.each(data.record, function ( index , value ){
+          $('.senderCitySearch').append('<option value="' + value.CITY_ID + '">' + value.CITY + '</option>')
+        })
+      }
+    })
+  }) // END OF ON CHANGE COUNTRY EVENT
+
+  
+
+
+}) // END OF MAIN DOCUMENT FUNCTION
